e661dd4756073f55a2b3e1e86972ebfadfafb455
Danila-Shuvaev <80268297+Danila-Shuvaev@users.noreply.github.com>
2021-07-23 14:58:00 +0300

    1

--- HMI/hmi_pub.py
+++ HMI/hmi_pub.py
@@ -1,13 +1,37 @@
 import random
-import paho.mqtt.client as mqtt
-import time
+
+from paho.mqtt import client as mqtt_client
 
 broker = 'broker.emqx.io'
-client = mqtt.Client("HMI for robot control")
-client.connect(broker)
+port = 1883
+topic = "hmi_pub"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = 'NDD55'
+password = 'NDD55'
 
-def publish(msg):
-    client.publish("commands", msg)
-    print("publish_" + msg + "_now")
-    time.sleep(1)
 
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("коннект с mqtt broker!")
+        else:
+            print("ошибка соединения %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client, msg):
+    msg = f"messages: top = {msg}"
+    result = client.publish(topic, msg)
+    print(f"Send `{msg}` to topic `{topic}`")
+
+
+
+def start():
+    client = connect_mqtt()
+    client.loop_start()
